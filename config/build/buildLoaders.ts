68871c74ba394ea1d692a4750795f4e0f9904d16
vlad <zakrevskiyvlada@yandex.ru>
2023-08-23 01:39:24 +0400

    upgrading of proj

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,31 +2,14 @@ import webpack from 'webpack'
 import { BuildOptions } from './types/config'
 import { buildCssLoader } from './loaders/buildCssLoader'
 import { buildSvgLoader } from './loaders/buildSvgLoader'
+import { buildBabelLoader } from './loaders/buildBabelLoader'
 
 export function buildLoaders({
     isDev,
 }: BuildOptions): webpack.RuleSetRule[] {
     const svgLoader = buildSvgLoader()
 
-    const babelLoader = {
-        test: /\.(js|jsx|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env'],
-                plugins: [
-                    [
-                        'i18next-extract',
-                        {
-                            locales: ['ru', 'en'],
-                            keyAsDefaultValue: true,
-                        },
-                    ],
-                ],
-            },
-        },
-    }
+    const babelLoader = buildBabelLoader(isDev)
 
     const cssLoader = buildCssLoader(isDev)
 
---
+++ config/build/loaders/buildBabelLoader.ts
@@ -0,0 +1,21 @@
+export const buildBabelLoader = (isDev: boolean) => ({
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+        loader: 'babel-loader',
+        options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+                [
+                    'i18next-extract',
+                    {
+                        locales: ['ru', 'en'],
+                        keyAsDefaultValue: true,
+                    },
+                ],
+                isDev &&
+                    require.resolve('react-refresh/babel'),
+            ].filter(Boolean),
+        },
+    },
+})
