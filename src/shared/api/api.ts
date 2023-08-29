850aee55f18b7e0f9811824f511cc430d2928c35
vlad <zakrevskiyvlada@yandex.ru>
2023-08-29 18:30:07 +0400

    fix api ( interceptor )

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -4,8 +4,16 @@ import { USER_LOCAL_STORAGE_KEY } from 'shared/consts/localStorage'
 export const $api = axios.create({
     baseURL: 'https://ulbi-course-server.vercel.app',
     headers: {
-        authorization: localStorage.getItem(
-            USER_LOCAL_STORAGE_KEY
-        ),
+        authorization:
+            localStorage.getItem(USER_LOCAL_STORAGE_KEY) ||
+            '',
     },
+})
+
+$api.interceptors.request.use((config) => {
+    if (config.headers) {
+        config.headers.Authorization =
+            localStorage.getItem(USER_LOCAL_STORAGE_KEY) ||
+            ''
+    }
 })
